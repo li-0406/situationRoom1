72e72110f9add8a3c642a155f92da26fdf3af09a
li-0406 <cr870406@gmail.com>
2023-07-19 20:05:58 +0800

    finish

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  // base: "/situationRoom1/",
   publicPath: process.env.NODE_ENV === "production" ? "/situationRoom1/" : "/",
 });
