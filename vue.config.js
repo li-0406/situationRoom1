b08ea9655742ed3438622d4d98593d5271b76943
li.0406 <cr870406@gmail.com>
2023-07-19 16:53:54 +0800

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  base: "/situationRoom1/",
+  // base: "/situationRoom1/",
   publicPath: process.env.NODE_ENV === "production" ? "/situationRoom1/" : "/",
 });
